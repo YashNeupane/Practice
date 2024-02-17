adeff14a4fbdf67cf3db593770205901d9c1757f
YashNeupane <128945418+YashNeupane@users.noreply.github.com>
2024-02-17 16:43:26 -0600

    Delete testinstall.py

--- testinstall.py
+++
@@ -1,3 +0,0 @@
-user_name = input('Please enter your full name separated by spaces only: ')
-len_name = len(user_name)
-print('Hello {}. Your name contains {} characters'.format(user_name, len_name))
